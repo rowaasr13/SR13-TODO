7b903bc3f6a38017ef324732ecd214972b509d9a
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-10-10 16:44:54 +0300

    use new filtering mechanism to collapse "Aiding the Accord" into one line when it picked up/completed

--- objectives.dragonflight.reputation.lua
+++ objectives.dragonflight.reputation.lua
@@ -1,23 +1,26 @@
 local a_name, a_env = ...
 
 local table_merge_shallow_left = _G["SR13-Lib"].table_utils.table_merge_shallow_left
+local table_add_pairs = _G["SR13-Lib"].pair_utils.table_add_pairs
+local pairs_get_vals = _G["SR13-Lib"].pair_utils.pairs_get_vals
 
 local objectives_branch = a_env.objectives.reputation
 
-objectives_branch.valdrakken_noevent = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 70750 } })
-objectives_branch.valdrakken_dragonbane = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 72374 } })
-objectives_branch.valdrakken_zskera = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 75259 } })
-objectives_branch.valdrakken_dreamsurge = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 77976 } })
-a_env.objectives.valdrakken_heroic = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 76122, progress = a_env.GetObjectiveQuestSingleObjectiveProgressString } }) -- Complete 5 Heroic, Fighting is its own reward
+local valdrakken_accord = {
+   { valdrakken_noevent = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 70750 } }) },
+   { valdrakken_dragonbane = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 72374 } }) },
+   { valdrakken_zskera = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 75259 } }) },
+   { valdrakken_dreamsurge = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 77976 } }) },
+}
+
+table_add_pairs(objectives_branch, valdrakken_accord)
+
 objectives_branch.loamm_niffen = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 75665, progress = a_env.GetObjectiveQuestSingleObjectiveProgressString } })
+a_env.objectives.valdrakken_heroic = table_merge_shallow_left({ a_env.weekly_quest_template, { quest_id = 76122, progress = a_env.GetObjectiveQuestSingleObjectiveProgressString } }) -- Complete 5 Heroic, Fighting is its own reward
 
 function a_env.OutputTableDragonflightReputation()
-   local output_table = a_env.CalculateObjectivesToOutputTable({
-      objectives_branch.valdrakken_noevent,
-      objectives_branch.valdrakken_dragonbane,
-      objectives_branch.valdrakken_zskera,
-      objectives_branch.valdrakken_dreamsurge,
-   })
+   local output_table = a_env.CalculateObjectivesToOutputTable(pairs_get_vals(valdrakken_accord))
+   output_table.all_completed = { name = output_table[1].name, state = "completed", period = output_table[1].period }
 
    a_env.OutputTableLeaveOnlyActiveQuest(output_table)
 
