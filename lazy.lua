be29a27f32edca543b81581f9b3bbe03b62133b7
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-10-13 13:33:45 +0300

    on-demand debug for lazy values on specific objectives

--- lazy.lua
+++ lazy.lua
@@ -3,8 +3,13 @@ local a_name, a_env = ...
 function a_env.GetLazy(tbl, key)
    local res
    local val = tbl[key]
+   local val_fixed = val
    assert(val ~= nil, ("value for key %q is nil"):format(key))
    if type(val) == "function" then res, val = val(tbl) end
+
+   if type(tbl.debug) == 'table' then
+      print(tbl, key, val_fixed, val)
+   end
 
    return val
 end
