4109dd9b1f05df9aa3baa1f00ade8271febe5f8e
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-10-27 08:01:33 +0300

    2nd argument to GetLazy now can be function, it will be called in same manner as if it was stored in value and cached according to same rules in table slot using this function reference as key

--- lazy.lua
+++ lazy.lua
@@ -1,11 +1,33 @@
 local a_name, a_env = ...
 
+local type = type
+
+-- Lazily "gets" value from table
+-- In simplest case returns value residing in tbl at key.
+-- If that value is a function, tries to run that function to get actual value with table as argument.
+-- If key is a function (and there previously was no fixed/cached value returned), tries to run this function to get actual value with table as argument.
+-- 1st return signifies caching mode, 2nd - actual value, result is cached as fixed (non-function) value in table with at slot key if it fits caching mode.
+-- Returns actual value
 function a_env.GetLazy(tbl, key)
-   local res
    local val = tbl[key]
    local val_fixed = val
-   assert(val ~= nil, ("value for key %q is nil"):format(key))
-   if type(val) == "function" then res, val = val(tbl) end
+
+   local key_is_func
+   if val == nil then
+      if type(key) == "function" then key_is_func = true else assert(false, ("value for key %q is nil"):format(key)) end
+   end
+
+   local res
+
+   if type(val) == "function" then
+      res, val = val(tbl)
+   elseif key_is_func then
+      res, val = key(tbl)
+   end
+
+   if res == "cachenonnil" and val ~= nil then
+      tbl[key] = val
+   end
 
    if type(tbl.debug) == 'table' then
       print(tbl, key, val_fixed, val)
