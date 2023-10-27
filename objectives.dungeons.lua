d748e35f07aabb2c4ff09bc0774903487080c713
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-10-27 15:59:40 +0300

    add Hallow's End objectives and move event dungeons support to separate file

--- objectives.dungeons.lua
+++ objectives.dungeons.lua
@@ -1,5 +1,7 @@
 local a_name, a_env = ...
 
+local table_merge_shallow_left = _G["SR13-Lib"].table_utils.table_merge_shallow_left
+
 local function GetLFGRandomDungeonInfoByDungeonId(required_dungeon_id) -- only returns info for dungeons available to player
    for idx = 1, GetNumRandomDungeons() do
       local id, name = GetLFGRandomDungeonInfo(idx)
@@ -24,3 +26,16 @@ function a_env.GetObjectiveLFGRandomFirstRewardCompleted(self)
    local doneToday, moneyAmount, moneyVar, experienceGained, experienceVar, numRewards, spellID = GetLFGDungeonRewards(self.lfg_dungeon_id)
    if doneToday then return "ok", true end
 end
+
+local event_dungeon_objective_template = {
+   name = a_env.GetObjectiveLFGRandomDungeonName,
+   state = a_env.GetObjectiveStateDefault,
+   available = a_env.GetObjectiveLFGRandomDungeonAvailable,
+   completed = a_env.GetObjectiveLFGRandomFirstRewardCompleted,
+   period = "daily dungeon",
+}
+
+a_env.objectives.event_brewfest = a_env.objectives.event_brewfest or {}
+a_env.objectives.event_brewfest.dungeon_coren = table_merge_shallow_left({ event_dungeon_objective_template, { lfg_dungeon_id = 287 } })
+a_env.objectives.event_hallowsend = a_env.objectives.event_hallowsend or {}
+a_env.objectives.event_hallowsend.dungeon_headless_horseman = table_merge_shallow_left({ event_dungeon_objective_template, { lfg_dungeon_id = 285 } })
--- objectives.event.brewfest.lua
+++ objectives.event.brewfest.lua
@@ -2,7 +2,7 @@ local a_name, a_env = ...
 
 local table_merge_shallow_left = _G["SR13-Lib"].table_utils.table_merge_shallow_left
 
-a_env.objectives.event_brewfest = {}
+a_env.objectives.event_brewfest = a_env.objectives.event_brewfest or {}
 a_env.objectives.event_brewfest.valdrakken_barrels = table_merge_shallow_left({ a_env.daily_quest_template, { quest_id = 77208, info = "Valdrakken" } })
 a_env.objectives.event_brewfest.valdrakken_bubbles = table_merge_shallow_left({ a_env.daily_quest_template, { quest_id = 76591, info = "Valdrakken" } })
 
@@ -14,12 +14,13 @@ a_env.objectives.event_brewfest.orgrimmar_invasion_direbrew = table_merge_shallo
 
 a_env.objectives.event_brewfest.orgrimmar_racing = table_merge_shallow_left({ a_env.daily_quest_template, { quest_id = 29393 } })
 
-a_env.objectives.event_brewfest.dungeon_coren = {
-   lfg_dungeon_id = 287,
-
-   name = a_env.GetObjectiveLFGRandomDungeonName,
-   state = a_env.GetObjectiveStateDefault,
-   available = a_env.GetObjectiveLFGRandomDungeonAvailable,
-   completed = a_env.GetObjectiveLFGRandomFirstRewardCompleted,
-   period = "daily dungeon",
-}
+   -- tooltip:AddSeparator()
+   -- tooltip:AddHeader("Brewfest", nil, ("tokens: %d"):format(GetItemCount(37829, true, true, true)))
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.valdrakken_barrels)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.valdrakken_bubbles)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.orgrimmar_bark_tchali)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.orgrimmar_bark_drohn)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.orgrimmar_invasion_direbrew)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.orgrimmar_invasion_hozen)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.orgrimmar_racing)
+   -- AddSingleObjectiveLine(tooltip, a_env.objectives.event_brewfest.dungeon_coren)
