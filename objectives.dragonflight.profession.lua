398ae486ca5560f4bb55bff174a7088349f28d91
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-11-01 21:16:27 +0300

    some more raw profession data before reorganization

--- objectives.dragonflight.profession.lua
+++ objectives.dragonflight.profession.lua
@@ -77,6 +77,7 @@ local source_data = {
    },
    [Enum.Profession.Herbalism] = {
       valdrakken_weekly = {
+         { bubble = { quest_id = 70614 } },
          { saxifrage = { quest_id = 70615 } },
          { hochenblume = { quest_id = 70616 } },
       },
@@ -89,10 +90,11 @@ local source_data = {
    [Enum.Profession.Leatherworking] = {
       valdrakken_weekly = {
          { herbalist_basket = { quest_id = 70569 } },
+         { trailblazers_bracers = { quest_id = 70568, name = a_env.GetObjectiveQuestNameAnd1stObjective } },--/dump GetItemInfo(193393)
       },
-      valdrakken_orders = { quest_id = 70594 },
-      loamm_weekly = {
-         { mycelium = { quest_id = 75354 } },
+      valdrakken_orders = { only_one = { quest_id = 70594 } },
+      factions_weekly = {
+         { niffen_mycelium = { quest_id = 75354 } },
       },
       item_weekly = {
          { loot_1 = { quest_id = 66384, item_id = 193910, loot_source = "Treasures", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
@@ -108,55 +110,89 @@ local source_data = {
          { mana_potion = { quest_id = 70531 } },
          { healing_potion = { quest_id = 70532 } },
       },
-      loamm_weekly = {
-         { deepflayers = { quest_id = 75363 } },
+      factions_weekly = {
+         { maruuk_lashers = { quest_id = 72427 } },
+         { niffen_deepflayers = { quest_id = 75363 } },
+      },
+      item_weekly = {
+         { loot_1 = { quest_id = 66373, item_id = 193891, loot_source = "Treasures", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
       },
    },
    [Enum.Profession.Tailoring] = {
       valdrakken_weekly = {
          { surveyor_robe = { quest_id = 70572 } },
+         { wildercloth_bolt = { quest_id = 70582 } },
          { chef_hat = { quest_id = 70586 } },
          { simple_reagent_bag = { quest_id = 70587 } },
       },
       item_weekly = {
          { loot_66386 = { quest_id = 66386, name = GetNameFromQuestID } },
          { loot_66387 = { quest_id = 66387, name = GetNameFromQuestID } },
-         { loot_70524 = { quest_id = 70524, name = GetNameFromQuestID,  info = "Weave" } }, -- {name="Ohn'arhan Weave - Nokhud Enemies (Tailoring)", quests={70524}, optionKey="tailoring", skillID=197},
-         { loot_70525 = { quest_id = 70525, name = GetNameFromQuestID, info = "Stupid" } }, -- {name="Stupidly Effective Stitchery - Gnoll Enemies (Tailoring)", quests={70525}, optionKey="tailoring", skillID=197},
+         { loot_3 = { quest_id = 70524, item_id = 198977, loot_source = "Nokhud", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
+         { loot_4 = { quest_id = 70525, item_id = 198978, loot_source = "Gnolls", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
          { treatise =   { quest_id = 74115, name = GetNameFromQuestID, info = "Treatise" } }, -- {name="Stupidly Effective Stitchery - Gnoll Enemies (Tailoring)", quests={70525}, optionKey="tailoring", skillID=197},
           -- {name="Valdrakken Profession Quests (Tailoring)", quests={72410, 70587, 66952, 70586, 70572, 70582, 66953, 66899}, optionKey="tailoring", skillID=197, useWorldState=true},
           -- {name="Crafting Order Quest (Tailoring)", quests={70595}, optionKey="tailoring", skillID=197},
       },
-      valdrakken_orders = { quest_id = 70595 },
-      loamm_weekly = {
-         { silk = { quest_id = 75600 } },
+      valdrakken_orders = { only_one = {  quest_id = 70595 } },
+      factions_weekly = {
+         { niffen_cocoon = { quest_id = 75407 } },
+         { niffen_silk = { quest_id = 75600 } },
       },
    },
    [Enum.Profession.Inscription] = {
       valdrakken_weekly = {
+         { fastened_quill = { quest_id = 70559 } },
          { explorer_compendium = { quest_id = 70560 } },
          { blazing_ink = { quest_id = 70561 } },
       },
-      -- loamm_proclamation = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75573, profession = Enum.Profession.Inscription } })
+      item_weekly = {
+         { loot_70518 = { quest_id = 70518, item_id = 198971, loot_source = "Djaradin", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
+         { loot_70519 = { quest_id = 70519, item_id = 198972, name = GetNameFromItemID, info = "Dragonkin/Drachtyr/Tarasek" } },
+      }
+      -- niffen_proclamation = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75573, profession = Enum.Profession.Inscription } })
+    -- {name="Disturbed Dirt or Expedition Scout's Pack (Inscription)", quests={66375, 66376}, optionKey="inscription", skillID=773},
+    -- {name="Curious Djaradin Rune - Djaradin Enemies (Inscription)", quests={70518}, optionKey="inscription", skillID=773},
+    -- {name="Draconic Treatise (Inscription)", quests={74105}, optionKey="inscription", skillID=773},
+    -- {name="Valdrakken Profession Quests (Inscription)", quests={66943, 66944, 70559, 70561, 70558, 70560, 66945, 72438}, optionKey="inscription", skillID=773, useWorldState=true},
+    -- {name="Crafting Order Quest (Inscription)", quests={70592}, optionKey="inscription", skillID=773},
    },
    [Enum.Profession.Jewelcrafting] = {
       valdrakken_weekly = {
          { pendant = { quest_id = 70565 } },
       },
-      valdrakken_orders = { quest_id = 70593 },
-      -- loamm_proclamation = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75573, profession = Enum.Profession.Inscription } })
-      -- a_env.objectives.profession_jewelcrafting.loamm_whelkshell = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75362, profession = Enum.Profession.Jewelcrafting } })
+      valdrakken_orders = { only_one = {  quest_id = 70593 } },
+      item_weekly = {
+         { loot_3 = { quest_id = 70521, item_id = 198974, loot_source = "Nokhud/Sundered Flame", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
+      },
 
+      -- niffen_proclamation = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75573, profession = Enum.Profession.Inscription } })
+      -- a_env.objectives.profession_jewelcrafting.niffen_whelkshell = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75362, profession = Enum.Profession.Jewelcrafting } })
+      -- {name="Disturbed Dirt or Expedition Scout's Pack (Jewelcrafting)", quests={66388, 66389}, optionKey="jewelcrafting", skillID=755},
+      -- {name="Incandescent Curio - Elemental Enemies (Jewelcrafting)", quests={70520}, optionKey="jewelcrafting", skillID=755},
+      -- {name="Draconic Treatise (Jewelcrafting)", quests={74112}, optionKey="jewelcrafting", skillID=755},
+      -- {name="Valdrakken Profession Quests (Jewelcrafting)", quests={66516, 70565, 66950, 66949, 72428, 70564, 70563, 70562}, optionKey="jewelcrafting", skillID=755, useWorldState=true},
+      -- {name="Crafting Order Quest (Jewelcrafting)", quests={70593}, optionKey="jewelcrafting", skillID=755},
    },
    [Enum.Profession.Blacksmithing] = {
       valdrakken_weekly = {
          { explorer_boots = { quest_id = 70211 } },
+         { draconium_axe = { quest_id = 70233 } },
          { draconium_hammer = { quest_id = 70234 } },
       },
-      loamm_weekly = {
+      item_weekly = {
+         { loot_1 = { quest_id = 66381, item_id = 192131, loot_source = "Treasures", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
+         { loot_2 = { quest_id = 66382, item_id = 192132, loot_source = "Treasures", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
+      },
+      factions_weekly = {
          { lava_blades = { quest_id = 75148 } },
       },
-      -- a_env.objectives.profession_blacksmithing.loamm_plate = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75569, profession = Enum.Profession.Blacksmithing } })
+    -- {name="Molten Globule - Rousing Fire Enemies (Blacksmithing)", quests={70513}, optionKey="blacksmithing", skillID=164},
+    -- {name="Primeval Earth Fragment - Rousing Earth Enemies (Blacksmithing)", quests={70512}, optionKey="blacksmithing", skillID=164},
+    -- {name="Draconic Treatise (Blacksmithing)", quests={74109}, optionKey="blacksmithing", skillID=164},
+    -- {name="Valdrakken Profession Quests (Blacksmithing)", quests={66941, 70233, 66517, 66897, 70235, 72398, 70234, 70211}, optionKey="blacksmithing", skillID=164, useWorldState=true},
+    -- {name="Crafting Order Quest (Blacksmithing)", quests={70589}, optionKey="blacksmithing", skillID=164},
+      -- a_env.objectives.profession_blacksmithing.niffen_plate = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75569, profession = Enum.Profession.Blacksmithing } })
       -- a_env.objectives.profession_blacksmithing.orders = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 70589, profession = Enum.Profession.Blacksmithing } })
    },
    [Enum.Profession.Enchanting] = {
@@ -165,16 +201,16 @@ local source_data = {
          { bracer_leech = { quest_id = 72173 } },
       },
       item_weekly = {
-         { loot_66377 = { quest_id = 66377, name = GetNameFromQuestID } },
-         { loot_66378 = { quest_id = 66377, name = GetNameFromQuestID } },
+         { loot_1 = { quest_id = 66377, item_id = 193900, loot_source = "Treasures", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
+         { loot_2 = { quest_id = 66378, item_id = 193901, loot_source = "Treasures", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
          { loot_70514 = { quest_id = 70514, name = GetNameFromQuestID, info = "Primordial Aether - Arcane Enemies (i.e. Academy)" } },
-         { loot_70515 = { quest_id = 70515, name = GetNameFromQuestID, info = "Primalist Charm - Humanoid Primalist Enemies (Druids of Fire?)" } },
+         { loot_4 = { quest_id = 70515, item_id = 198968, loot_source = "Primalists (and Druids of Flame?)", name = GetObjectiveProfessionLootName, state = GetObjectiveProfessionLootProgress } },
          -- { treatise =   { quest_id = 74115, name = GetNameFromQuestID, info = "Treatise" } }, -- {name="Stupidly Effective Stitchery - Gnoll Enemies (Tailoring)", quests={70525}, optionKey="tailoring", skillID=197},
       },
-      loamm_weekly = {
+      factions_weekly = {
          { fireshards = { quest_id = 75150 } },
       },
-      -- a_env.objectives.profession_enchanting.loamm_relic = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75865, profession = Enum.Profession.Enchanting } })
+      -- a_env.objectives.profession_enchanting.niffen_relic = table_merge_shallow_left({ weekly_profession_quest_template, { quest_id = 75865, profession = Enum.Profession.Enchanting } })
     -- {name="Draconic Treatise (Enchanting)", quests={74110}, optionKey="enchanting", skillID=333},
     -- {name="Valdrakken Profession Quests (Enchanting)", quests={66884, 66900, 66935, 72155, 72172, 72173, 72175, 72423}, optionKey="enchanting", skillID=333, useWorldState=true},
    },
