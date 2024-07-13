bc480f670567d274a8b0147a8342eaf87e827cea
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2024-07-13 04:05:22 +0300

    clean-up unnecessary iterations over all dungeons where info can just be queried directly; AUTOLOCAL

--- objectives.dungeons.lua
+++ objectives.dungeons.lua
@@ -1,25 +1,23 @@
 local a_name, a_env = ...
 
+-- [AUTOLOCAL START]
+local GetLFGDungeonInfo = GetLFGDungeonInfo
+local GetLFGDungeonRewards = GetLFGDungeonRewards
+local GetLFGMode = GetLFGMode
+local IsLFGDungeonJoinable = IsLFGDungeonJoinable
+local _G = _G
+-- [AUTOLOCAL END]
+
 local table_merge_shallow_left = _G["SR13-Lib"].table_utils.table_merge_shallow_left
 
-local function GetLFGRandomDungeonInfoByDungeonId(required_dungeon_id) -- only returns info for dungeons available to player
-   for idx = 1, GetNumRandomDungeons() do
-      local id, name = GetLFGRandomDungeonInfo(idx)
-      if id == required_dungeon_id then
-         return id, name, IsLFGDungeonJoinable(id)
-      end
-   end
-end
-
 function a_env.GetObjectiveLFGRandomDungeonName(self)
-   local id, name, joinable = GetLFGRandomDungeonInfoByDungeonId(self.lfg_dungeon_id)
-   return "ok", name
+   local name = GetLFGDungeonInfo(self.lfg_dungeon_id)
+   return "cachenonempty", name
 end
 
 function a_env.GetObjectiveLFGRandomDungeonAvailable(self)
-   local id, name, joinable = GetLFGRandomDungeonInfoByDungeonId(self.lfg_dungeon_id)
+   local joinable = IsLFGDungeonJoinable(self.lfg_dungeon_id)
    if joinable then return "ok", "IsLFGDungeonJoinable" end
-   -- if C_UnitAuras.GetPlayerAuraBySpellID(self.aura_spell_id) then return "ok", "GetPlayerAuraBySpellID" end
 end
 
 function a_env.GetObjectiveLFGRandomFirstRewardCompleted(self)
