e4824daf3277d3db0f6fee22766dba3e1bb6b73f
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2025-02-23 16:58:08 +0300

    timewalking: turns out there are several turn-in quests too - at least one for each Alliance and Horde - provide support for setting multiple turn-in quest ids and item ids too; add data for Cataclysm week

--- objectives.timewalking.lua
+++ objectives.timewalking.lua
@@ -33,24 +33,46 @@ local timewalking_raid_quest_template = table_merge_shallow_left({ a_env.weekly_
 } })
 
 local timewalking = {
-   { classic         = { quest_id = { 85947 }, lfg_dungeon_id = 2634, aura_spell_id = 452307, item_turn_in_item_id = 225348, item_turn_in_quest_id = 83285 } },
-   { burning_crusade = { quest_id = { 85948, 72727 }, lfg_dungeon_id =  744, aura_spell_id = 335148, item_turn_in_item_id = 129747, item_turn_in_quest_id = 40168 } },
-   { lichking        = { quest_id = { 85949 }, lfg_dungeon_id =  995, aura_spell_id = 335149, item_turn_in_item_id = 129928, item_turn_in_quest_id = 40173 } },
-   { cataclysm       = { quest_id = { 72810, 86556 }, lfg_dungeon_id = 1146, aura_spell_id = 335150, item_turn_in_item_id = 133377, item_turn_in_quest_id = 40786, raid_quest_id = 57637 } },
-   { pandaria        = { quest_id = { 72725, 86560 }, lfg_dungeon_id = 1453, aura_spell_id = 335151, item_turn_in_item_id = 143776, item_turn_in_quest_id = 45563 } },
-   { wod             = { quest_id = { 72724, 86563 }, lfg_dungeon_id = 1971, aura_spell_id = 335152, item_turn_in_item_id = 167922, item_turn_in_quest_id = 55499 } },
-   { legion          = { quest_id = { 72719, 86564 }, lfg_dungeon_id = 2274, aura_spell_id = 359082, item_turn_in_item_id = 187611, item_turn_in_quest_id = 64710 } },
+   { classic         = { quest_id = { 85947 },        lfg_dungeon_id = 2634, aura_spell_id = 452307, item_turn_in_item_id = { 225348 },         item_turn_in_quest_id = { 83285 } } },
+   { burning_crusade = { quest_id = { 85948, 72727 }, lfg_dungeon_id =  744, aura_spell_id = 335148, item_turn_in_item_id = { 129747 },         item_turn_in_quest_id = { 40168 } } },
+   { lichking        = { quest_id = { 85949 },        lfg_dungeon_id =  995, aura_spell_id = 335149, item_turn_in_item_id = { 129928 },         item_turn_in_quest_id = { 40173 } } },
+   { cataclysm       = { quest_id = { 72810, 86556 }, lfg_dungeon_id = 1146, aura_spell_id = 335150, item_turn_in_item_id = { 133377, 133378 }, item_turn_in_quest_id = { 40786, 40787 }, raid_quest_id = 57637 } },
+   { pandaria        = { quest_id = { 72725, 86560 }, lfg_dungeon_id = 1453, aura_spell_id = 335151, item_turn_in_item_id = { 143776 },         item_turn_in_quest_id = { 45563 } } },
+   { wod             = { quest_id = { 72724, 86563 }, lfg_dungeon_id = 1971, aura_spell_id = 335152, item_turn_in_item_id = { 167922 },         item_turn_in_quest_id = { 55499 } } },
+   { legion          = { quest_id = { 72719, 86564 }, lfg_dungeon_id = 2274, aura_spell_id = 359082, item_turn_in_item_id = { 187611 },         item_turn_in_quest_id = { 64710 } } },
 }
 
-for idx, key, val in array_of_pairs_iter(timewalking) do
+local function MakeQuestGroup(args)
+   local quest_ids = args.quest_ids
+   local key_prefix = args.key_prefix
+   local template = args.template
+   local quest_start_item_ids = args.quest_start_item_ids
+
    local out = {}
-   val.group_dungeon_quest = out
-   for quest_id_idx = 1, #val.quest_id do
-      out[#out + 1] = { ['dungeon_' .. quest_id_idx] = table_merge_shallow_left({ timewalking_quest_template, val, { quest_id = val.quest_id[quest_id_idx]} }) }
+   for quest_id_idx = 1, #quest_ids do
+      local quest_data = table_merge_shallow_left({ template, { quest_id = quest_ids[quest_id_idx]} })
+      if quest_start_item_ids then quest_data.quest_start_item_id = quest_start_item_ids[quest_id_idx] end
+      out[#out + 1] = { [key_prefix .. quest_id_idx] = quest_data }
    end
 
+   return out
+end
+
+for idx, key, val in array_of_pairs_iter(timewalking) do
+   val.group_dungeon_quest = MakeQuestGroup({
+      quest_ids = val.quest_id,
+      key_prefix = 'dungeon_',
+      template = table_merge_shallow_left({ timewalking_quest_template, val })
+   })
+
+   val.group_item_turn_in_quest = MakeQuestGroup({
+      quest_ids = val.item_turn_in_quest_id,
+      quest_start_item_ids = val.item_turn_in_item_id,
+      key_prefix = 'item_turn_in_',
+      template = table_merge_shallow_left({ timewalking_token_turnin_quest_template, val })
+   })
+
    -- not a group, just a single-element array
-   val.item_turn_in_quest = { table_merge_shallow_left({ timewalking_token_turnin_quest_template, val, { quest_id = val.item_turn_in_quest_id, quest_start_item_id = val.item_turn_in_item_id } }) }
    if val.raid_quest_id then
       val.raid_quest = { table_merge_shallow_left({ timewalking_raid_quest_template, val, { quest_id = val.raid_quest_id } }) }
    end
@@ -66,7 +88,9 @@ function a_env.OutputTablesTimewalking()
       a_env.OutputTableLeaveOnlyActiveQuest(output_table)
       output_tables[#output_tables + 1] = output_table
 
-      output_table = a_env.CalculateObjectivesToOutputTable(val.item_turn_in_quest)
+      output_table = a_env.CalculateObjectivesToOutputTable(val.group_item_turn_in_quest, "array_of_pairs")
+      output_table.none_active = { name = output_table[1].name, info = output_table[1].info, state = output_table[1].state, period = output_table[1].period }
+      a_env.OutputTableLeaveOnlyActiveQuest(output_table)
       output_tables[#output_tables + 1] = output_table
 
       if val.raid_quest then
